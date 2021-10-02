7c889bf2d1ebeaefa72a06a0fba7c83340cf4232
SANEDROID123 <62328357+prajwal080501@users.noreply.github.com>
2021-10-02 17:23:05 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "@mui/material";
+import "./App.css";
+import { useState } from "react";
+import "./index.css";
+
+// import Input from "@mui/material/Input";
+const api = {
+  key: "d5c2642b92a84bb35b072f49153c44e6",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+          console.log(result);
+        });
+    }
+  };
+
+  const getDate = new Date();
+  const currentDate = getDate.toDateString();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? "App warm" : "App") : "App" }>
+        <main>
+          <div className="search-box">
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Search by city name.."
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+            ></input>
+          </div>
+          {typeof weather.main != "undefined" ? (
+            <div>
+              <div className="location-box">
+                <div className="location">
+                  {weather.name} {weather.sys.country}
+                </div>
+                <div className="date">{currentDate}</div>
+              </div>
+              <div className="weather">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather-box">{weather.weather[0].main}</div>
+              <div className="note">All data displayed here is realtime <b>Weather App</b>  All Rights Reserved ©
+ 2021 <br/> <br /> <i> Developed By Prajwal Ladkat</i></div>
+            </div>
+          ) : (
+            ""
+          )}
+          
+
+  
+          
+        </main>
+      </div>
+    </Container>
   );
 }
 
